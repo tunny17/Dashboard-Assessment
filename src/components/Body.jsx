fdd624d614bddefc5a7519dee3ac807cb75cea64
tunny17 <tvnji17@gmail.com>
2023-04-18 21:10:50 +0100

    Completion of the mock todo card

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Plus from '../assets/Hero-Nav/Plus.svg';
-import cardImage from '../assets/CardImage.png';
 
-import { AiOutlineMore } from 'react-icons/ai';
-import { FiLink2 } from 'react-icons/fi';
-
-import List from '../assets/List.svg';
-import Comment from '../assets/Comment.svg';
-import User from '../assets/User.svg';
-
-
-import Box from '@mui/material/Box'
-import Card from '@mui/material/Card';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-
-import Slider from '@mui/material/Slider';
+import TodoCard from './TodoCard';
 
 
 const Body = () => {
+
+  const [todo, setTodo] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try{
+        const { data: { todo } } = await axios.get('data.json');
+        setTodo(todo);
+        console.log(todo);
+      }catch(error){
+        console.log(error)
+      }
+    })()
+  }, []);
+
+
   return (
     <div className='body-container'>
 
@@ -52,44 +54,12 @@ const Body = () => {
             To Do
           </div>
 
-          <Card sx={{ maxWidth: 266.67 }}>
+          <div className='flex flex-col'>
+            {
+              todo.map((todo) => <TodoCard key={todo.id} todo={todo} /> )
+            }
+          </div>
 
-            <CardMedia component='img' height='194' sx={{ padding: '5%' }} image={cardImage} alt='test' />
-
-            <CardContent>
-
-              <Typography variant='subtitle1'>
-                <Box display='flex' alignItems='center' justifyContent='space-between'>
-                  <span style={{ fontWeight: 600 }}>Highfidelity Design</span> <AiOutlineMore style={{ color: '#B8B9BD' }}/>
-                </Box>
-              </Typography>
-
-              <Typography variant='subtitle2'>
-                <span style={{ fontSize: '14px', color: '#B8B9BD', fontWeight: 400 }}>Make clear design and color</span>
-              </Typography>
-
-              <Box display='flex' justifyContent='space-between' marginTop={1}>
-                <Box display='flex' alignItems='center'>
-                  <img src={List} alt="" style={{ marginRight: '5px', width: '100%' }}/> <span style={{ fontSize: '14px', fontWeight: 400, color: '#B8B9BD' }}>Progress</span>
-                </Box>
-                <span style={{ fontSize: '14px', fontWeight: 500 }}>2/10</span>
-              </Box>
-
-              <Slider defaultValue={50} aria-label="Disabled slider" className="slider"   sx={{ color: 'red', height: '8px' }} />
-
-              <Box display='flex' alignItems='center' justifyContent='space-between' marginTop={1}>
-                <Box display='flex' alignItems='center'>
-                  <Box display='flex' alignItems='center' marginRight='15px'>
-                    <img src={Comment} alt="" style={{ marginRight: '4px', width: '16px' }} /> <span style={{ fontSize: '12px', fontWeight: 400, color: '#B8B9BD' }}>7</span>
-                  </Box>
-                  <Box display='flex' alignItems='center'>
-                    <FiLink2 style={{ color: '#B8B9BD', marginRight: '4px', width: '16px' }} /> <span style={{ fontSize: '12px', fontWeight: 400, color: '#B8B9BD' }}>2</span>
-                  </Box>
-                </Box>
-                <img src={User} alt="" />
-              </Box>
-            </CardContent>
-          </Card>
         </div>
         
       </div>
---
+++ src/components/TodoCard.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+
+import { AiOutlineMore } from 'react-icons/ai';
+import { FiLink2 } from 'react-icons/fi';
+
+import List from '../assets/List.svg';
+import Comment from '../assets/Comment.svg';
+import User from '../assets/User.svg';
+
+
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
+import Slider from '@mui/material/Slider';
+
+const TodoCard = ({todo}) => {
+
+    const { id, subtitle1, subtitle2, image, color } = todo;
+
+
+
+
+
+    return (
+    <div>
+        <Card sx={{ maxWidth: 266.67, maxHeight: 446 }}>
+
+            <CardMedia component='img' height='194' sx={{ padding: '5% 5% 0 5%' }} image={image} alt='test' />
+
+            <CardContent>
+
+            <Typography variant='subtitle1'>
+                <Box display='flex' alignItems='center' justifyContent='space-between'>
+                    <span style={{ fontWeight: 600 }}>{subtitle1}</span> <AiOutlineMore style={{ color: '#B8B9BD' }}/>
+                </Box>
+            </Typography>
+
+            <Typography variant='subtitle2'>
+                <span style={{ fontSize: '14px', color: '#B8B9BD', fontWeight: 400 }}>{subtitle2}</span>
+            </Typography>
+
+            <Box display='flex' justifyContent='space-between' marginTop={1}>
+                <Box display='flex' alignItems='center'>
+                    <img src={List} alt="" style={{ marginRight: '5px', width: '100%' }}/> <span style={{ fontSize: '14px', fontWeight: 400, color: '#B8B9BD' }}>Progress</span>
+                </Box>
+                <span style={{ fontSize: '14px', fontWeight: 500 }}>2/10</span>
+            </Box>
+
+            <Slider defaultValue={50} aria-label="Disabled slider" className="slider"   sx={{ color: {color}, height: '8px' }} />
+
+            <Box display='flex' alignItems='center' justifyContent='space-between'>
+                <Box display='flex' alignItems='center'>
+                    <Box display='flex' alignItems='center' marginRight='15px'>
+                        <img src={Comment} alt="" style={{ marginRight: '4px', width: '16px' }} /> <span style={{ fontSize: '12px', fontWeight: 400, color: '#B8B9BD' }}>7</span>
+                    </Box>
+                    <Box display='flex' alignItems='center'>
+                        <FiLink2 style={{ color: '#B8B9BD', marginRight: '4px', width: '16px' }} /> <span style={{ fontSize: '12px', fontWeight: 400, color: '#B8B9BD' }}>2</span>
+                    </Box>
+                </Box>
+                <img src={User} alt="" />
+            </Box>
+
+            </CardContent>
+
+        </Card>
+    </div>
+    )
+}
+
+export default TodoCard
